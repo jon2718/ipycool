5f84d9fbb619f17606a0f33e520a13a43b8f9779
Jon Lederman <jon@thesoniccloud.com>
2014-11-22 22:51:55 -0500

    Mods to CONT

--- ipycool.py
+++ ipycool.py
@@ -800,158 +800,212 @@ class Cont(ICoolObject):
                      'req': False,
                      'default': 1E-6},
 
-        'ffcr':     {'default': False,
-                     'desc': '(L) if .true. => inserts form feed and carriage returns in the output log file so there '
+        'ffcr':     {'desc': '(L) if .true. => inserts form feed and carriage returns in the output log file so there '
                      'are two plots per page starting at the top of a page',
+                     'doc': '',
                      'type': 'Logical',
-                     'req': False},
+                     'req': False,
+                     'default': False},
 
-        'forcerp':  {'default': True,
-                     'desc': '(L) if .true. => set x, y, Px, and Py for reference particle to 0 for each new REFP '
+        'forcerp':  {'desc': '(L) if .true. => set x, y, Px, and Py for reference particle to 0 for each new REFP '
                      'command and for each ACCEL region with phasemodel=4.',
+                     'doc': '',
                      'type': 'Logical',
-                     'req': False},
+                     'req': False,
+                     'default': True},
 
-        'fsav':     {'default': None,
-                     'desc': '(L) if .true. => store particle info at plane IZFILE into file FOR004.DAT. (false). '
+        'fsav':     {'desc': '(L) if .true. => store particle info at plane IZFILE into file FOR004.DAT. (false). '
                      'It is possible to get the initial distribution of particles that get a given error flag be '
                      'setting the plane=IFAIL . It is possible to get the initial distribution of particles that '
                      'successfully make it to the end of the simulation by setting the plane= -1.',
+                     'doc': '',
                      'type': 'Logical',
-                     'req': False},
+                     'req': False,
+                     'default': None},
 
-        'fsavset':  {'default': False,
-                     'type': '(L) if .true. => modify data stored using FSAV in FOR004.DAT to have z=0 and '
+        'fsavset':  {'desc': '(L) if .true. => modify data stored using FSAV in FOR004.DAT to have z=0 and '
                      'times relative to reference particle at plane IZFILE.',
+                     'doc': '',
                      'type': 'Logical',
-                     'req': False},
+                     'req': False,
+                     'default': False},
 
-        'f9dp':     {'default': None,
-                     'desc': '(I) number of digits after the decimal point for floating point variables in FOR009.DAT '
+        'f9dp':     {'desc': '(I) number of digits after the decimal point for floating point variables in FOR009.DAT '
                      '{4,6,8,10,12,14,16,17} (4) F9DP=17 gives 16 digits after the decimal point and 3 digits in the '
                      'exponent',
+                     'doc': '',
                      'type': 'Integer',
-                     'req': False},
+                     'req': False,
+                     'default': None},
 
-        'goodtrack': {'default': True,
-                      'desc': '(L) if .true. and BGEN=.false. => only accepts input data from file FOR003.DAT if '
+        'goodtrack': {'desc': '(L) if .true. and BGEN=.false. => only accepts input data from file FOR003.DAT if '
                       'IPFLG=0.; if .false. => resets IPFLG of bad input tracks to 0 (this allows processing a '
                       'file of bad tracks for diagnostic purposes)',
+                      'doc': '',
                       'type': 'Logical',
-                      'req': False},
+                      'req': False,
+                      'default': True},
 
-        'izfile':    {'default': None,
-                      'desc': '(I) z-plane where particle info is desired when using FSAV. Use 1 to store beam at '
+        'izfile':    {'desc': '(I) z-plane where particle info is desired when using FSAV. Use 1 to store beam at '
                       'production. Saves initial particle properties for bad tracks if IZFILE=IFAIL #.  Saves initial '
                       'particle properties for tracks that get to the end of the simulation if IZFILE=-1.  IZFILE '
                       'should point to the end of a REGION or to an APERTURE , ROTATE or TRANSPORT pseudoregion '
                       'command.',
+                      'doc': '',
                       'type': 'Integer',
-                      'req': False},
+                      'req': False,
+                      'default': None},
 
-        'magconf':    {'default': 0,
-                       'desc': '(I) if 19 < MAGCONF=mn < 100 => reads in file FOR0mn.DAT, which contains data on '
+        'magconf':    {'desc': '(I) if 19 < MAGCONF=mn < 100 => reads in file FOR0mn.DAT, which contains data on '
                        'solenoidal magnets. Used with SHEET, model 4.',
+                       'doc': '',
                        'type': 'Integer',
-                       'req': False},
+                       'req': False,
+                       'default': 0},
 
-        'mapdef':     {'default': 0,
-                       'desc': '(I) if 19 < MAPDEF=mn < 100 => reads in file FOR0mn.DAT, which contains data on how '
+        'mapdef':     {'desc': '(I) if 19 < MAPDEF=mn < 100 => reads in file FOR0mn.DAT, which contains data on how '
                        'to set up field grid. Used with SHEET, model 4.',
+                       'doc': '',
                        'type': 'Integer',
-                       'req': False},
+                       'req': False,
+                       'default': 0},
 
-        'neighbor':   {'default': False,
-                       'desc': "(L) if .true. => include fields from previous and following regions when calculating "
+        'neighbor':   {'desc': "(L) if .true. => include fields from previous and following regions when calculating "
                        "field.  This parameter can be used with soft-edge fields when the magnitude of the "
                        "field doesn't fall to 0 at the region boundary. A maximum of 100 region can be used "
                        "with this feature.",
+                       'doc': '',
                        'type': 'Logical',
-                       'req': False},
+                       'req': False,
+                       'default': False},
 
-        'neutrino':    {'default': 0,
-                        'desc': '(I) if 19 < NEUTRINO=mn < 100 => writes out file FOR0mn.DAT, which contains '
+        'neutrino':    {'desc': '(I) if 19 < NEUTRINO=mn < 100 => writes out file FOR0mn.DAT, which contains '
                         'neutrino production data. See section 5.2 for the format.',
+                        'doc': '',
                         'type': 'Integer',
-                        'req': False},
+                        'req': False,
+                        'default': 0},
 
-        'nnudk':       {'default': 1,
-                        'desc': '(I) # of neutrinos to produce at each muon, pion and kaon decay.',
+        'nnudk':       {'desc': '(I) # of neutrinos to produce at each muon, pion and kaon decay.',
+                        'doc': '',
                         'type': 'Integer',
-                        'req': False},
+                        'req': False,
+                        'default': 1},
 
-        'npart':       {'default': None,
-                        'desc': '(I) # of particles in simulation. The first 300,000 particles are stored in memory. '
+        'npart':       {'desc': '(I) # of particles in simulation. The first 300,000 particles are stored in memory. '
                         'Larger numbers are allowed in principle since ICOOL writes the excess particle '
                         'information to disc. However, there can be a large space and speed penalty in doing '
                         'so.',
+                        'doc': '',
                         'type': 'Integer',
-                        'req': False},
+                        'req': False,
+                        'default': None},
 
-        'nprnt':        {'default': -1,
-                         'desc': ' Number of diagnostic events to print out to log file.',
+        'nprnt':        {'desc': ' Number of diagnostic events to print out to log file.',
+                         'doc': '',
                          'type': 'Integer',
-                         'req': False},
+                         'req': False,
+                         'default': -1},
 
-        'npskip':       {'default': 0,
-                         'desc': 'Number of input particles in external beam file to skip before processing starts',
+        'npskip':       {'desc': 'Number of input particles in external beam file to skip before processing starts',
+                         'doc': '',
                          'type': 'Integer',
-                         'req': False},
+                         'req': False,
+                         'default': 0},
 
-        'nsections':    {'default': 1,
-                         'desc': '(I) # of times to repeat basic cooling section (1).  This parameter can be used to '
+        'nsections':    {'desc': '(I) # of times to repeat basic cooling section (1).  This parameter can be used to '
                          'repeat all the commands between the SECTION and ENDSECTION commands in the problem '
                          'definition. If a REFP command immediately follows the SECTION command, it is not '
                          'repeated',
+                         'doc': '',
                          'type': 'Integer',
-                         'req': False},
+                         'req': False,
+                         'default': 1},
 
-        'ntuple':        {'default': False,
-                          'desc': '(L) if .true. => store information about each particle after every region in file '
+        'ntuple':        {'desc': '(L) if .true. => store information about each particle after every region in file '
                           'FOR009.DAT. This variable is forced to be false if RTUPLE= true.(false)}',
+                          'doc': '',
                           'type': 'Logical',
-                          'req': False},
+                          'req': False,
+                          'default': False},
 
-        'nuthmin':      {'default': 0,
-                         'desc': '(R) Minimum polar angle to write neutrino production data to file. [radians]',
+        'nuthmin':      {'desc': '(R) Minimum polar angle to write neutrino production data to file. [radians]',
+                         'doc': '',
                          'type': 'Real',
-                         'req': False},
+                         'req': False,
+                         'default': 0},
 
-        'nuthmax':      {'default': 3.14,
-                         'desc': 'Maximum polar angle to write neutrino production data to file. [radians]',
+        'nuthmax':      {'desc': 'Maximum polar angle to write neutrino production data to file. [radians]',
+                         'doc': '',
                          'type': 'Real',
-                         'req': False},
+                         'req': False,
+                         'default': 3.14},
 
-        'output1':      {'default': False,
-                         'desc': 'if .true. => write particle information at production (plane 1) to the '
+        'output1':      {'desc': 'if .true. => write particle information at production (plane 1) to the '
                          'postprocessor output file for009.dat.',
+                         'doc': '',
                          'type': 'Logical',
-                         'req': False},
-      
-        'phantom':     {'default': False,
-                        'desc': 'if .true. => force particle to keep initial transverse coordinates after every '
-                                '(L) if .true. => force particle to keep initial transverse coordinates after '
-                                'every step. This is useful for making magnetic field maps. (false)',
-                        'type': 'Logical',
-                        'req': False},
-                                   
-        'phasemodel':   {'default': 1,
-                         'desc': 'PHASEMODEL (I) controls how the phase is determined in rf cavities. (1) '
-                                 '1: takes phase directly from ACCEL command [degrees] '
-                                 '2 - 6: takes phase model from REFP command '
-                                 '7: reads phases in from file FOR0mn.DAT, where RFPHASE=mn. See sec. 5.1.},',
-                         'type': 'Integer',
-                         'req': False},
+                         'req': False,
+                         'default': False},
 
-        'prlevel': {},
-                
-        'prnmax': {},
-                
-        'pzmintrk': {},
-                
-        'rfdiag': {},
-                
-        'rfphase': {},
+        'phantom':     {'desc': 'if .true. => force particle to keep initial transverse coordinates after every '
+                        '(L) if .true. => force particle to keep initial transverse coordinates after '
+                        'every step. This is useful for making magnetic field maps. (false)',
+                        'doc': '',
+                        'type': 'Logical',
+                        'req': False,
+                        'default': False},
+
+        'phasemodel':   {'desc': 'PHASEMODEL (I) controls how the phase is determined in rf cavities. (1) '
+                         '1: takes phase directly from ACCEL command [degrees] '
+                         '2 - 6: takes phase model from REFP command '
+                         '7: reads phases in from file FOR0mn.DAT, where RFPHASE=mn. See sec. 5.1.},',
+                         'doc': '',
+                         'type': 'Integer',
+                         'req': False,
+                         'default': 1},
+
+        'prlevel':       {'desc': 'Controls level of print information to log file (for NPRINT events);higher # '
+                                  'gives more print(1)',
+                          'doc': '1: values at end of region '
+                                 '2: + values at end of each time step '
+                                 '3: + E,B values at each step '
+                                 '4: + information in cylindrical coordinates',
+                          'type': 'Integer',
+                          'req': False,
+                          'default': 1,
+                          'min': 1,
+                          'max': 4},
+
+        'prnmax':         {'desc': 'Sets maximum number of steps to generate print out inside a region',
+                           'doc': '',
+                           'type': 'Integer',
+                           'req': 'False',
+                           'default': 300},
+
+        'pzmintrk':       {'desc': 'Sets the value of Pz below which tracking stops. [GeV/c]',
+                           'doc': '',
+                           'type': 'Real',
+                           'req': 'False',
+                           'default': 0.001},
+
+        'rfdiag':         {'desc': 'if 19 < RFDIAG=mn < 100 => writes rf diagnostic information at the '
+                                   'end of each accelerator region to file FOR0mn.DAT.',
+                           'doc': '',
+                           'type': 'Integer',
+                           'req': False,
+                           'default': 0,
+                           'min': 19,
+                           'max': 100},
+           
+        'rfphase':        {'desc': 'If PHASEMODEL=5 => reads rf phases, frequencies and gradients '
+                                   'for the cavities from file FOR0mn.DAT, where RFPHASE=mn '
+                                   'and 19 < mn < 100 (0)',
+                           'doc': '',
+                           'type': 'Integer',
+                           'default': 0,
+                           'min': 19,
+                           'max': 100},
         
         'rnseed': {},
         
